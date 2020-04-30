af14140b7ad3307e3d32a96920a8b0423ed520a7
Symeon Huang <hzwhuang@gmail.com>
2020-04-30 17:28:31 +0100

    Add a simple icon
    not a designer... but proundly made with free software

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QIcon>
 #include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
@@ -6,6 +7,7 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(QStringLiteral(":/kesdict-icon.svg")));
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/standalone.qml")));
